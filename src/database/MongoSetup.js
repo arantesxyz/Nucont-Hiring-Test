6d3a6b9e728bceb0470ad7d1bc0666e033823d4f
Gustavo Arantes <gustavolucio27@gmail.com>
2019-05-17 16:18:14 -0300

    Casos de teste adicionados, código reformulado para facilitar utilização.

--- src/database/MongoSetup.js
+++ src/database/MongoSetup.js
@@ -1,39 +1,48 @@
 const mongoose = require('mongoose');
 
-const dataSchema = new mongoose.Schema({
-    classifier: {
-        type: String,
-        require: false
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    openingBalance: {
-        type: Number,
-        required: false
-    },
-    debit: {
-        type: Number,
-        required: false
-    },
-    credit: {
-        type: Number,
-        required: false
-    },
-    finalBalance: {
-        type: Number,
-        required: false
-    },
-    parent: {
-        type: String,
-        required: false
-    },
-    access: {
-        type: String,
-        required: false
-    }
-});
+class MongoSetup {
+    constructor(collection){
+        this._collection = collection;
 
-const dbModel = mongoose.model('datas', dataSchema);
-module.exports = dbModel;
+        this.dataSchema = new mongoose.Schema({
+            classifier: {
+                type: String,
+                require: false
+            },
+            description: {
+                type: String,
+                required: false
+            },
+            openingBalance: {
+                type: Number,
+                required: false
+            },
+            debit: {
+                type: Number,
+                required: false
+            },
+            credit: {
+                type: Number,
+                required: false
+            },
+            finalBalance: {
+                type: Number,
+                required: false
+            },
+            parent: {
+                type: String,
+                required: false
+            },
+            access: {
+                type: String,
+                required: false
+            }
+        });
+    }
+
+    get model() {
+        return mongoose.model(this._collection, this.dataSchema);
+    }
+
+}
+module.exports = MongoSetup;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,96 @@
-const reqFile = require('../src/file');
+const assert = require('chai').assert;
+const file1 = require('../out/teste1.json');
+const file2 = require('../out/teste2.json');
+const file3 = require('../out/teste3.json');
+const file4 = require('../out/teste4.json');
 
-// Getting the input file
-const File = new reqFile('/../files/level3.txt');
+describe('Length of JSON array', () => {
+    it('length of level 1 should return 4', () => {
+        assert.equal(file1.length, 4);
+    });
 
-// Return the formated data
-File.getCollection().then((data) => {
-    console.log('foi');
+    it('length of level 2 should return 43', () => {
+        assert.equal(file2.length, 43);
+    });
+
+    it('length of level 3 should return 39', () => {
+        assert.equal(file3.length, 39);
+    });
+
+    it('length of level 4 should return 59', () => {
+        assert.equal(file4.length, 59);
+    });
+
+});
+
+describe('Type of objects', () => {
+    it('Level 1 object type test', () => {
+        file1.forEach(e => {
+            assert.typeOf(e.description, 'string');
+            assert.typeOf(e.classifier, 'string');
+            assert.typeOf(e.openingBalance, 'number');
+            assert.typeOf(e.debit, 'number');
+            assert.typeOf(e.credit, 'number');
+            assert.typeOf(e.finalBalance, 'number');
+        });
+    });
+
+    it('Level 2 object type test', () => {
+        file2.forEach(e => {
+            assert.typeOf(e.description, 'string');
+            assert.typeOf(e.classifier, 'string');
+            assert.typeOf(e.openingBalance, 'number');
+            assert.typeOf(e.debit, 'number');
+            assert.typeOf(e.credit, 'number');
+            assert.typeOf(e.finalBalance, 'number');
+        });
+    });
+
+    it('Level 3 object type test', () => {
+        file3.forEach(e => {
+            assert.typeOf(e.description, 'string');
+            assert.typeOf(e.classifier, 'string');
+            assert.typeOf(e.openingBalance, 'number');
+            assert.typeOf(e.debit, 'number');
+            assert.typeOf(e.credit, 'number');
+            assert.typeOf(e.finalBalance, 'number');
+        });
+    });
+
+    it('Level 3 object type test', () => {
+        file4.forEach(e => {
+            assert.typeOf(e.description, 'string');
+            assert.typeOf(e.classifier, 'string');
+            assert.typeOf(e.openingBalance, 'number');
+            assert.typeOf(e.debit, 'number');
+            assert.typeOf(e.credit, 'number');
+            assert.typeOf(e.finalBalance, 'number');
+        });
+    });
+});
+
+describe('Classifier should be able to numberParse', () => {
+    it('Level 1 classifier parse test', () => {
+        file1.forEach(e => {
+            assert.typeOf(parseInt(e.classifier), 'number');
+        });
+    });
+
+    it('Level 2 classifier parse test', () => {
+        file2.forEach(e => {
+            assert.typeOf(parseInt(e.classifier), 'number');
+        });
+    });
+    
+    it('Level 2 classifier parse test', () => {
+        file2.forEach(e => {
+            assert.typeOf(parseInt(e.classifier), 'number');
+        });
+    });
+    
+    it('Level 2 classifier parse test', () => {
+        file2.forEach(e => {
+            assert.typeOf(parseInt(e.classifier), 'number');
+        });
+    });
 });
